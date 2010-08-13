ee487f5ebd53f1a8c5664fe6af82de1c0b8df874
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2010-08-13 03:08:16 +0200

    Some suport for atom, and autodetection.

--- erozja_parse.erl
+++ erozja_parse.erl
@@ -2,7 +2,7 @@
 
 -export([file/1, string/1]).
 
--record(state, {list=[], last_characters=undefined, level=1, stack=[]}).
+-record(state, {list=[], last_characters=undefined, level=1, stack=[], type, f=fun event/3}).
 
 -include("erozja.hrl").
 
@@ -32,7 +32,6 @@ to_records(List) ->
 to_date(D=undefined) ->
 	D;
 to_date(DateString) ->
-	% "Sun, 25 Jan 2009 19:20:02 +0000"
 	DateTime = httpd_util:convert_request_date(DateString),
 	Timestamp = calendar:datetime_to_gregorian_seconds(DateTime),
 	Timestamp.
@@ -40,53 +39,96 @@ to_date(DateString) ->
 
 % helper functions to track stack
 
-level_event(Event = {startElement, _, Name, _, _}, Location, State = #state{level=Level, stack=Stack}) ->
-	NewState = event(Event, Location, State#state{level=Level+1, stack=[Name | Stack]}),
+level_event(Event = {startElement, _, Name, _, _}, Location, State = #state{level=Level, stack=Stack, f=F}) ->
+	NewState = F(Event, Location, State#state{level=Level+1, stack=[Name | Stack]}),
 	NewState#state{level=Level+1, stack=[Name | Stack]};
-level_event(Event = {endElement, _, Name, _}, Location, State = #state{level=Level, stack=[Name | Stack]}) ->
-	NewState = event(Event, Location, State),
+level_event(Event = {endElement, _, Name, _}, Location, State = #state{level=Level, stack=[Name | Stack], f=F}) ->
+	NewState = F(Event, Location, State),
 	NewState#state{level=Level-1, stack=Stack};
 level_event(_Event = {endElement, _, Name, _}, _Location, _State) ->
 	throw({somethingwrong, Name});
-level_event(Event, Location, State = #state{level=Level, stack=Stack}) ->
-	NewState = event(Event, Location, State),
+level_event(Event, Location, State = #state{level=Level, stack=Stack, f=F}) ->
+	NewState = F(Event, Location, State),
 	NewState#state{level=Level, stack=Stack}.
-
 
 % 
 
 
--define(VALUE(ParentTitle, Title),
+-define(VALUE(Type, ParentTitle, Title),
 % For the start field, clean the characters buffer
-event(_Event = {startElement, _, _, _, _}, _Location, State = #state{stack=[Title, ParentTitle | _]}) ->
+Type(_Event = {startElement, _, _, _, _}, _Location, State = #state{stack=[Title, ParentTitle | _]}) ->
 	State#state{last_characters=undefined};
 %% For the end field event, use the last set of characters 
 %% encountered as the value for that field
-event(_Event = {endElement, _, _, _}, _Location, State = #state{stack=[Title, ParentTitle | _], list=[Item | Rest], last_characters=Chars}) ->
+Type(_Event = {endElement, _, _, _}, _Location, State = #state{stack=[Title, ParentTitle | _], list=[Item | Rest], last_characters=Chars}) ->
 	Updated = [{list_to_atom(string:to_lower(Title)), Chars} | Item],
 	State#state{list=[Updated|Rest], last_characters=undefined}
 ).
 % endof macro QUOTE_VALUE
 
-%% Start "FutureQuote" creates a new, empty key-value list
-%% for the quote
-event(_Event = {startElement, _, _, _, _}, _Location, _State = #state{stack = ["item" | _], list=Items}) ->
+
+% detection code
+
+event(_Event = {startElement, _, _, _, _}, _Location, State = #state{stack=["rss"]}) ->
+	io:format("rss~n"),
+	State#state{type=rss,f=fun rss_event/3};
+event(_Event = {startElement, _, _, _, _}, _Location, State = #state{stack=["feed"]}) ->
+	io:format("atom~n"),
+	State#state{type=atom,f=fun atom_event/3};
+%% Catch-all. Pass state on as-is
+event(_Event, _Location, State) ->
+	State.
+
+
+
+% rss specific codes
+
+rss_event(_Event = {startElement, _, _, _, _}, _Location, _State = #state{stack = ["item" | _], list=Items}) ->
 	#state{list=[[]|Items], last_characters=undefined};
 
 %% Characters are stores in the parser state
-event(_Event = {characters, Chars}, _Location, State = #state{}) ->
+rss_event(_Event = {characters, Chars}, _Location, State = #state{}) ->
 	State#state{last_characters=Chars};
 
-?VALUE("item", "title");
-?VALUE("item", "link");
-?VALUE("item", "description");
-?VALUE("item", "pubDate");
-?VALUE("item", "guid");
+?VALUE(rss_event, "item", "title");
+?VALUE(rss_event, "item", "link");
+?VALUE(rss_event, "item", "description");
+?VALUE(rss_event, "item", "guid");
+?VALUE(rss_event, "item", "pubDate");
+	% "Sun, 25 Jan 2009 19:20:02 +0000"
+	% but sometimes % "Sun, 1 Aug 2010 00:00:00 PST"
 
 % not interesting tags
-event(_Event = {endElement, _, _Other, _}, _Location, State = #state{}) ->
+rss_event(_Event = {endElement, _, _Other, _}, _Location, State = #state{}) ->
 	State#state{last_characters=undefined};
 
 %% Catch-all. Pass state on as-is
-event(_Event, _Location, State) ->
+rss_event(_Event, _Location, State) ->
+	State.
+
+
+% atom specific codes
+
+atom_event(_Event = {startElement, _, _, _, _}, _Location, _State = #state{stack = ["item" | _], list=Items}) ->
+	#state{list=[[]|Items], last_characters=undefined};
+
+%% Characters are stores in the parser state
+atom_event(_Event = {characters, Chars}, _Location, State = #state{}) ->
+	State#state{last_characters=Chars};
+
+?VALUE(atom_event, "entry", "title"); % type="xhtml"
+?VALUE(atom_event, "entry", "subtitle"); % type="xhtml"
+?VALUE(atom_event, "entry", "link");  % read href attribute
+?VALUE(atom_event, "entry", "content"); % type="xhtml"
+?VALUE(atom_event, "entry", "id");
+%?VALUE(atom_event, "entry", "author", "name");
+?VALUE(atom_event, "entry", "updated");
+	%  <updated>2010-06-09T04:19:58Z</updated>
+
+% not interesting tags
+atom_event(_Event = {endElement, _, _Other, _}, _Location, State = #state{}) ->
+	State#state{last_characters=undefined};
+
+%% Catch-all. Pass state on as-is
+atom_event(_Event, _Location, State) ->
 	State.
