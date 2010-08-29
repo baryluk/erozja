f24868e0c9d31d8d7c4a1d02da0edff471468282
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2010-08-29 03:23:09 +0200

    Add force_update_all function. Rename Pid to QueuePid.

--- erozja_manager.erl
+++ erozja_manager.erl
@@ -4,7 +4,7 @@
 
 -behaviour(gen_server).
 
--export([start/0, start_link/0, stop/0, add_feed/1, list_feeds/0]).
+-export([start/0, start_link/0, stop/0, add_feed/1, list_feeds/0, force_update_all/0]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
@@ -31,13 +31,17 @@ add_feed(URL) ->
 list_feeds() ->
 	gen_server:call(?REGC, list_feeds).
 
+force_update_all() ->
+	gen_server:cast(?REGC, force_update_all).
+
 
 init(noargs) ->
 	{ok, #state{feeds=[]}}.
 
+
 handle_call({add_feed, URL}, _From, State = #state{feeds=Feeds}) ->
-	{ok, Pid} = erozja_queues_sup:start_child(URL),
-	NewFeed = {URL, Pid},
+	{ok, QueuePid} = erozja_queues_sup:start_child(URL),
+	NewFeed = {URL, QueuePid},
 	NewState = State#state{feeds = [NewFeed | Feeds]},
 	Result = ok,
 	{reply, Result, NewState};
@@ -50,15 +54,25 @@ handle_call(Unknown, From, State) ->
 	io:format("~p: Unknown message ~p from ~p~n", [?MODULE, Unknown, From]),
 	{noreply, State}.
 
+
 handle_cast(stop, State) ->
 	{stop, stop, State};
+
+handle_cast(force_update_all, State = #state{feeds=Feeds}) ->
+	lists:foreach(fun({_, QueuePid}) ->
+		erozja_queue:force_update(QueuePid)
+	end, Feeds),
+	{noreply, State};
+
 handle_cast(Unknown, State) ->
 	io:format("~p: Unknown message ~p~n", [?MODULE, Unknown]),
 	{noreply, State}.
 
+
 handle_info(Msg, State) ->
 	io:format("~p: Unknown message ~p~n", [?MODULE, Msg]),
 	{noreply, State}.
+
 
 terminate(_Reason, _State) ->
 	ok.
