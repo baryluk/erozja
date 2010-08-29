cb7c00c08f772db4f3233c1a6d0747e8d3079a12
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2010-08-29 14:38:49 +0200

    Many changes: detect duplicated/already present entries, notify about errors, do not send all items to subscribers, add measuring time of loader, etc.

--- erozja_queue.erl
+++ erozja_queue.erl
@@ -9,7 +9,7 @@
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--record(state, {url, items=[], title, favicon=undefined, count_total=0, count_unreaded=0, tref=undefined, update_interval=60, last_update=0, loader_pid, loader_monitor, type, subscribed_to=[], subscribed_by=[]}).
+-record(state, {url, items=[], title, favicon=undefined, count_total=0, count_unreaded=0, tref=undefined, update_interval=600, last_update=0, loader_pid, loader_monitor, loader_started, type, subscribed_to=[], subscribed_by=[], added=0}).
 
 
 -define(deb_enable, 1).
@@ -31,7 +31,8 @@ start_link() ->
 stop(Pid) ->
 	gen_server:cast(Pid, stop).
 
-
+% todo: limit number of items (sort by date)
+% todo: option to not send content (only important metadata)
 get_items(Pid) ->
 	gen_server:call(Pid, get_items).
 
@@ -58,6 +59,13 @@ subscribe(Pid, Data) ->
 	Self = self(),
 	gen_server:call(Pid, {subscribe, Self, Data}).
 
+store(Pid, Item, Opts) ->
+	gen_server:call(Pid, {store, Item, Opts}).
+
+% opts:
+%   flagged
+%   {tag, Tg}
+%   seen
 
 init({url, URL}) ->
 	State0 = #state{url=URL, type=feed, title=URL},
@@ -106,12 +114,12 @@ handle_call(Unknown, From, State) ->
 %   started_update
 %   updated
 
-handle_cast({add_item, Item}, State = #state{subscribed_by = Clients}) ->
+handle_cast({add_item, Item}, State = #state{}) ->
 	State1 = add(Item, State),
-	notify({add_item, Item}, Clients),
 	{noreply, State1};
-handle_cast({loader_done, LoaderPid, Status}, State = #state{subscribed_by = Clients, loader_pid=LoaderPid}) ->
-	notify({update_done, LoaderPid, Status}, Clients),
+handle_cast({loader_done, LoaderPid, Status}, State = #state{subscribed_by = Clients, loader_pid=LoaderPid, loader_started=LoaderStarted}) ->
+	Diff = timer:now_diff(now(), LoaderStarted) div 1000,
+	notify({update_done, LoaderPid, Diff, Status}, Clients),
 	{noreply, State};
 handle_cast(stop, State) ->
 	{stop, stop, State};
@@ -121,9 +129,10 @@ handle_cast(Unknown, State) ->
 
 start_update(State = #state{url=URL, loader_pid=undefined, subscribed_by = Clients}) ->
 	{LoaderPid, LoaderMonitor} = spawn_monitor(erozja_loader, url, [URL, self()]),
+	Now = now(),
 	?deb("Update for ~p started.~n", [URL]),
 	notify({update_started, LoaderPid}, Clients),
-	State#state{loader_pid=LoaderPid, loader_monitor=LoaderMonitor};
+	State#state{loader_pid=LoaderPid, loader_monitor=LoaderMonitor, loader_started=Now};
 start_update(State = #state{url=URL,loader_pid=OldLoader}) when is_pid(OldLoader) ->
 	?deb("Update for ~p already in progress.~n", [URL]),
 	State.
@@ -133,20 +142,22 @@ handle_info(update_by_timer, State0) ->
 	State1 = start_update(State0),
 	State2 = set_timer(State1),
 	{noreply, State2};
-handle_info({'DOWN', MonitorRef, process, LoaderPid, Reason}, State1 = #state{loader_pid = LoaderPid, loader_monitor=MonitorRef, url=URL, subscribed_by = Clients}) ->
+handle_info({'DOWN', MonitorRef, process, LoaderPid, Reason}, State1 = #state{loader_pid = LoaderPid, loader_monitor=MonitorRef, loader_started=LoaderStarted, url=URL, subscribed_by = Clients, added=Added}) ->
+	Now = now(),
+	Diff = timer:now_diff(Now, LoaderStarted) div 1000,
 	case Reason of
 		normal ->
 			% normal not nacassarly mean, that it is done without error
-			NewItemsCount = 0,
-			notify({update_done, LoaderPid, {ok, NewItemsCount}}, Clients),
+			NewItemsCount = Added,
+			notify({update_done, LoaderPid, Diff, {ok, NewItemsCount}}, Clients),
 			ok;
 		_ ->
-			notify({update_done, LoaderPid, Reason}, Clients),
+			notify({update_done, LoaderPid, Diff, Reason}, Clients),
 			?deb("Loader ~p for ~p down:~n Reason ~p~n", [LoaderPid, URL, Reason]),
 			ok
 	end,
 	State2 = set_timer(State1),
-	State3 = State2#state{loader_pid=undefined, loader_monitor=undefined},
+	State3 = State2#state{loader_pid=undefined, loader_monitor=undefined, added=0, last_update=Now},
 	{noreply, State3};
 handle_info({'DOWN', _MonitorRef, process, Pid, Reason}, State1 = #state{subscribed_by = Clients, url = URL}) ->
 	?deb("Subscriber ~p for ~p down:~n Reason ~p~n", [Pid, URL, Reason]),
@@ -167,8 +178,38 @@ code_change(_OldVsn, State, _Extra) ->
 get_items0(_State = #state{items=Items}) ->
 	Items.
 
-add(Item, State = #state{items=Items}) ->
-	State#state{items = [Item | Items]}.
+add(Item, State = #state{items=Items, subscribed_by=Clients, added=Added}) ->
+	case is_already(Item, Items) of
+		false ->
+			State1 = State#state{items = [Item | Items], added=Added+1},
+			notify({new_item, Item}, Clients),
+			State1;
+		%{updated, Position} ->
+		%	
+		true ->
+			State
+	end.
+
+% todo: updated entries, removed entries?
+is_already(_Item1 = #rss_item{guid=GUID1, link=Link1, title=Title1, pubdate=_PubDate1}, Items) ->
+	lists:any(fun(_Item2 = #rss_item{guid=GUID2, link=Link2, title=Title2, pubdate=_PubDate2}) ->
+		case {GUID1, GUID2} of
+			{X1, X2} when (X1 =/= undefined) and (X2 =/= undefined) ->
+				X1 =:= X2;
+			_ ->
+				case {Link1, Link2} of
+					{Y1, Y2} when (Y1 =/= undefined) and (Y2 =/= undefined) ->
+						Y1 =:= Y2;
+					_ ->
+						case {Title1, Title2} of
+							{Z1, Z2} when (Z1 =/= undefined) and (Z2 =/= undefined) ->
+								Z1 =:= Z2;
+							_ ->
+								false
+						end
+				end
+		end
+	end, Items).
 
 get_summary(#state{title = Title, favicon = Favicon, count_total = CountTotal, count_unreaded = CountUnreaded, last_update = LastUpdate}) ->
 	{Title, Favicon, CountTotal, CountUnreaded, LastUpdate}.
